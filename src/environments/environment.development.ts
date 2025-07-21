352b2f5029a44de708185138f1ce286bdb0c2648
HemanthSaiTeja <hemanthsaiteja881@gmail.com>
2025-07-21 15:27:47 +0530

    "env"

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,6 @@ export const environment = {
   weatherapi:
     'http://api.weatherapi.com/v1',
     weatherApiKey:'1f60664b4d1241ada57102712251607',
-    normalfile:'Hemanth',
-    normalfilew:'Hemanth'
+   
 
 };
